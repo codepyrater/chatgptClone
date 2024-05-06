668a71f419bdc7ca99de8569c42015a4768d761d
codepyrater <punna1s@cmich.edu>
2024-05-06 15:07:03 -0400

    completed UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,18 @@ import msgIcon from '../src/assets/message.svg'
 import home from '../src/assets/home.svg'
 import saved from '../src/assets/bookmark.svg'
 import rocket from '../src/assets/rocket.svg'
+import sendBtn  from '../src/assets/send.svg'
+import userIcon from '../src/assets/user-icon.png'
+import gptImgLogo from '../src/assets/chatgptLogo.svg'
+
+
+
 function App() {
   return (
     <div className="App">
       <div className="sideBar">
         <div className="upperSide">
-          <div className="uppserSideTop"><img src= {logo}alt ="logo" className='logo' /> <span className="brand">chatGpt</span></div>
+          <div className="uppserSideTop"><img src= {logo}alt ="logo" className='logo' /> <span className="brand">ChatGPT</span></div>
             <button className="midBtn"><img src = {addBtn} alt = "New chat" className='addBtn'/>New Chat</button>
             <div className="upperSideBottom">
               <button className="query"><img src={msgIcon} alt = "Query"/>what is this?</button>
@@ -20,12 +26,28 @@ function App() {
           <div className="lowerSide">
           
               <div className="listItems"><img src ={home} alt = "Home" className='listitemsImg' />Home</div>
-              <div className="listItems"><img src={saved} alt = "Saved" className='listitemsImg' />Save</div>
+              <div className="listItems"><img src={saved} alt = "Saved" className='listitemsImg' />Saved</div>
               <div className="listItems"><img src={rocket} alt = "Rocket" className='listitemsImg' />Upgrade to Pro</div>
 
           </div>
         </div>
         <div className="main">
+          <div className="chats">
+            <div className="chat">
+              <img className='chatImg' src={userIcon}alt = "ChatGPT"/><p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolore nemo delectus ratione placeat vero incidunt, dicta voluptatum eaque quo id illum eius! Debitis doloribus quasi rerum molestiae nostrum harum.</p>
+            </div>
+            <div className="chat bot">
+              <img className='chatImg' src={gptImgLogo}alt = "ChatGPT"/><p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolore nemo delectus ratione placeat vero incidunt, dicta voluptatum eaque quo id illum eius! Debitis doloribus quasi rerum molestiae nostrum harum.</p>
+            </div>
+
+          </div>
+
+          <div className="chatFooter">
+            <div className="inp">
+              <input type='text' placeholder = 'Send a message'/> <button className="send"><img src={sendBtn} alt= "Send" /></button>
+            </div>
+            <p>ChatGPT May produce incorrect Information</p>
+          </div>
 
         </div>
       </div>
